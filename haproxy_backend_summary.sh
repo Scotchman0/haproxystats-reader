f212c9400349e8069eb3b9c998ace6bfad381399
Will Russell <will.russell.01@gmail.com>
2022-05-30 10:30:05 -0400

    formatting update

--- haproxy_backend_summary.sh
+++ haproxy_backend_summary.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-haproxy_1=haproxy.config_default
-haproxy_2=haproxy.config_shard
+# haproxy config baseline output script for reviewing a general overview of the expected workload on a haproxy config
 
-echo "default haproxy cluster totals Dynamic-cookie-key | server-template | be_http | be_tcp | be_sni | be_no_sni | be_edge_http"
-cat $haproxy_1 |  grep dynamic-cookie-key | wc -l
-cat $haproxy_1 | grep server-template | wc -l
-cat $haproxy_1 | grep be_http | wc -l
-cat $haproxy_1 | grep be_tcp | wc -l
-cat $haproxy_1 | grep be_sni | wc -l
-cat $haproxy_1 | grep be_no_sni | wc -l
-cat $haproxy_1 | grep be_edge_http | wc -l
+echo "haproxy backend report, please haproxy.config name"
+read $haproxy_1
 
-echo "sharded cluster totals Dynamic-cookie-key | server-template | be_http | be_tcp | be_sni | be_no_sni | be_edge_http"
-cat $haproxy_2 | grep dynamic-cookie-key | wc -l
-cat $haproxy_2 | grep server-template | wc -l
-cat $haproxy_2 | grep be_http | wc -l
-cat $haproxy_2 | grep be_tcp | wc -l
-cat $haproxy_2 | grep be_sni | wc -l
-cat $haproxy_2 | grep be_no_sni | wc -l
-cat $haproxy_2 | grep be_edge_http | wc -l
-
+echo "default haproxy cluster backend totals:"
+echo "Dynamic-cookie-key: " cat $haproxy_1 | grep dynamic-cookie-key | wc -l
+echo "server-templates: " cat $haproxy_1 | grep server-template | wc -l
+echo "be_http: " cat $haproxy_1 | grep be_http | wc -l
+echo "be_tcp: " cat $haproxy_1 | grep be_tcp | wc -l
+echo "be_sni: " cat $haproxy_1 | grep be_sni | wc -l
+echo "be_no_sni: " cat $haproxy_1 | grep be_no_sni | wc -l
+echo "be_edge_http: " cat $haproxy_1 | grep be_edge_http | wc -l
